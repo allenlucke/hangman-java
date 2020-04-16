2f0e4177df109bf5656505721506b093b218f55a
Allen Lucke <allenlucke@gmail.com>
2020-04-16 17:02:20 -0500

    added logic to prompter and isHit method to Hangman

--- src/main/java/com/project/Hangman/Hangman.java
+++ src/main/java/com/project/Hangman/Hangman.java
@@ -5,5 +5,12 @@ public class Hangman {
     public static void main(String[] args) {
 
         Game game = new Game("beginning");
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit) {
+            System.out.println("We have a hit!");
+        }else {
+            System.out.println("Missed!");
+        }
     }
 }
