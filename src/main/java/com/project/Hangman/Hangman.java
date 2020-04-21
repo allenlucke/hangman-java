688b5d59d24125d200e4d482171086440e7f4a40
Allen Lucke <allenlucke@gmail.com>
2020-04-21 16:44:09 -0500

    game will now take args as enterred in config

--- src/main/java/com/project/Hangman/Hangman.java
+++ src/main/java/com/project/Hangman/Hangman.java
@@ -3,8 +3,12 @@ import com.project.Hangman.Game;
 
 public class Hangman {
     public static void main(String[] args) {
-
-        Game game = new Game("beginning");
+        if(args.length == 0) {
+            System.out.println("Usage:  java Hangman <answer>");
+            System.err.println("answer is required");
+            System.exit(1);
+        }
+        Game game = new Game(args[0]);
         Prompter prompter = new Prompter(game);
         while(game.getRemainingTries() > 0 && !game.isWon()) {
             prompter.displayProgress();
